f3378113a6fd902b0b023bbc262f607352afdd70
Woojin Choi <cdw6791432@gmail.com>
2024-08-29 23:35:36 -0700

    2d

--- 2d.go
+++ 2d.go
@@ -11,15 +11,31 @@ import (
 	"golang.org/x/exp/rand"
 )
 
+type TerrainType struct {
+	Name   string
+	Height float64
+	Colour color.RGBA
+}
+
 type MapGenerator struct {
 	MapWidth   int
 	MapHeight  int
 	NoiseScale float64
+
+	Octaves     int
+	Persistance float64
+	Lacunarity  float64
+
+	Seed   uint64
+	Offset pixel.Vec
+
 	AutoUpdate bool
-	NoiseMap   [][]float64
+	Regions    []TerrainType
+
+	NoiseMap [][]float64
 }
 
-func (mg *MapGenerator) generateNoiseMap() {
+func (mg *MapGenerator) GenerateNoiseMap() {
 	mg.NoiseMap = make([][]float64, mg.MapWidth)
 	for i := range mg.NoiseMap {
 		mg.NoiseMap[i] = make([]float64, mg.MapHeight)
@@ -29,28 +45,144 @@ func (mg *MapGenerator) generateNoiseMap() {
 		mg.NoiseScale = 0.0001
 	}
 
-	seed := time.Now().UnixNano()
-	noise := opensimplex.New(rand.Int63n(seed))
+	seed := uint64(time.Now().UnixNano())
+	prng := rand.New(rand.NewSource(seed))
+	noise := opensimplex.New(int64(rand.Uint64()))
+
+	octaveOffsets := make([]pixel.Vec, mg.Octaves)
+	for i := 0; i < mg.Octaves; i++ {
+		offsetX := float64(prng.Intn(200000)-100000) + mg.Offset.X
+		offsetY := float64(prng.Intn(200000)-100000) + mg.Offset.Y
+		octaveOffsets[i] = pixel.V(offsetX, offsetY)
+	}
+
+	maxNoiseHeight := math.Inf(-1)
+	minNoiseHeight := math.Inf(1)
+
+	halfWidth := float64(mg.MapWidth) / 2.0
+	halfHeight := float64(mg.MapHeight) / 2.0
 
 	for y := 0; y < mg.MapHeight; y++ {
 		for x := 0; x < mg.MapWidth; x++ {
-			sampleX := float64(x) / mg.NoiseScale
-			sampleY := float64(y) / mg.NoiseScale
+			amplitude := 1.0
+			frequency := 1.0
+			noiseHeight := 0.0
 
-			perlinValue := noise.Eval2(sampleX, sampleY)
-			mg.NoiseMap[x][y] = (perlinValue + 1) / 2 // Normalize to [0, 1]
+			for i := 0; i < mg.Octaves; i++ {
+				sampleX := (float64(x)-halfWidth)/mg.NoiseScale*frequency + octaveOffsets[i].X
+				sampleY := (float64(y)-halfHeight)/mg.NoiseScale*frequency + octaveOffsets[i].Y
+
+				perlinValue := noise.Eval2(sampleX, sampleY)*2.0 - 1.0
+				noiseHeight += perlinValue * amplitude
+
+				amplitude *= mg.Persistance
+				frequency *= mg.Lacunarity
+			}
+
+			if noiseHeight > maxNoiseHeight {
+				maxNoiseHeight = noiseHeight
+			}
+			if noiseHeight < minNoiseHeight {
+				minNoiseHeight = noiseHeight
+			}
+			mg.NoiseMap[x][y] = noiseHeight
+		}
+	}
+
+	for y := 0; y < mg.MapHeight; y++ {
+		for x := 0; x < mg.MapWidth; x++ {
+			mg.NoiseMap[x][y] = (mg.NoiseMap[x][y] - minNoiseHeight) / (maxNoiseHeight - minNoiseHeight)
 		}
 	}
 }
 
-func (mg *MapGenerator) drawNoiseMap(win *pixelgl.Window) {
-	pic := pixel.MakePictureData((pixel.R(0, 0, float64(mg.MapWidth), float64(mg.MapHeight))))
+func (mg *MapGenerator) GenerateNoiseMap_island() {
+	mg.NoiseMap = make([][]float64, mg.MapWidth)
+	for i := range mg.NoiseMap {
+		mg.NoiseMap[i] = make([]float64, mg.MapHeight)
+	}
+
+	if mg.NoiseScale <= 0 {
+		mg.NoiseScale = 0.0001
+	}
+
+	seed := uint64(time.Now().UnixNano())
+	prng := rand.New(rand.NewSource(seed))
+	noise := opensimplex.New(int64(rand.Uint64()))
+
+	octaveOffsets := make([]pixel.Vec, mg.Octaves)
+	for i := 0; i < mg.Octaves; i++ {
+		offsetX := float64(prng.Intn(200000)-100000) + mg.Offset.X
+		offsetY := float64(prng.Intn(200000)-100000) + mg.Offset.Y
+		octaveOffsets[i] = pixel.V(offsetX, offsetY)
+	}
+
+	maxNoiseHeight := math.Inf(-1)
+	minNoiseHeight := math.Inf(1)
+
+	halfWidth := float64(mg.MapWidth) / 2.0
+	halfHeight := float64(mg.MapHeight) / 2.0
+	maxDistance := math.Sqrt(halfWidth*halfWidth + halfHeight*halfHeight)
+
+	for y := 0; y < mg.MapHeight; y++ {
+		for x := 0; x < mg.MapWidth; x++ {
+			amplitude := 1.0
+			frequency := 1.0
+			noiseHeight := 0.0
+
+			for i := 0; i < mg.Octaves; i++ {
+				sampleX := (float64(x)-halfWidth)/mg.NoiseScale*frequency + octaveOffsets[i].X
+				sampleY := (float64(y)-halfHeight)/mg.NoiseScale*frequency + octaveOffsets[i].Y
+
+				perlinValue := noise.Eval2(sampleX, sampleY)*2.0 - 1.0
+				noiseHeight += perlinValue * amplitude
+
+				amplitude *= mg.Persistance
+				frequency *= mg.Lacunarity
+			}
+
+			distance := math.Sqrt(math.Pow(float64(x)-halfWidth, 2) + math.Pow(float64(y)-halfHeight, 2))
+			gradient := 1.0 - (distance / maxDistance)
+			noiseHeight *= gradient
+
+			if noiseHeight > maxNoiseHeight {
+				maxNoiseHeight = noiseHeight
+			}
+			if noiseHeight < minNoiseHeight {
+				minNoiseHeight = noiseHeight
+			}
+			mg.NoiseMap[x][y] = noiseHeight
+		}
+	}
+
+	for y := 0; y < mg.MapHeight; y++ {
+		for x := 0; x < mg.MapWidth; x++ {
+			mg.NoiseMap[x][y] = (mg.NoiseMap[x][y] - minNoiseHeight) / (maxNoiseHeight - minNoiseHeight)
+		}
+	}
+}
+
+func (mg *MapGenerator) DrawNoiseMap(win *pixelgl.Window, drawMode string) {
+	pic := pixel.MakePictureData(pixel.R(0, 0, float64(mg.MapWidth), float64(mg.MapHeight)))
 
 	for y := 0; y < mg.MapHeight; y++ {
 		for x := 0; x < mg.MapWidth; x++ {
 			noiseValue := mg.NoiseMap[x][y]
-			gray := uint8(math.Floor(noiseValue * 255))
-			col := color.RGBA{R: gray, G: gray, B: gray, A: 255}
+			var col color.RGBA
+
+			switch drawMode {
+			case "NoiseMap":
+				gray := uint8(math.Floor(noiseValue * 255))
+				col = color.RGBA{R: gray, G: gray, B: gray, A: 255}
+			case "ColourMap":
+				for _, region := range mg.Regions {
+					if noiseValue <= region.Height {
+						col = region.Colour
+						break
+					}
+				}
+			}
+
 			pic.Pix[y*mg.MapWidth+x] = col
 		}
 	}
@@ -72,24 +204,44 @@ func run() {
 		panic(err)
 	}
 
-	/* Init value */
 	mapGen := &MapGenerator{
-		MapWidth:   500,
-		MapHeight:  500,
-		NoiseScale: 100.0,
-		AutoUpdate: false,
+		MapWidth:    500,
+		MapHeight:   500,
+		NoiseScale:  300,
+		Octaves:     5,
+		Persistance: 0.5,
+		Lacunarity:  2.0,
+		Seed:        0, //uint64(time.Now().UnixNano())
+		Offset:      pixel.V(0, 0),
+		Regions: []TerrainType{
+			{"Mountain2", 0.1, color.RGBA{R: 74, G: 89, B: 100, A: 255}},
+			{"Mountain", 0.2, color.RGBA{R: 63, G: 73, B: 75, A: 255}},
+			{"Ground", 0.3, color.RGBA{R: 79, G: 49, B: 1, A: 255}},
+			{"Grass2", 0.4, color.RGBA{R: 102, G: 136, B: 59, A: 255}},
+			{"Grass", 0.5, color.RGBA{R: 183, G: 183, B: 71, A: 255}},
+			{"Sand2", 0.6, color.RGBA{R: 183, G: 183, B: 71, A: 255}},
+			{"Sand", 0.7, color.RGBA{R: 242, G: 223, B: 152, A: 255}},
+			{"Water3", 0.8, color.RGBA{R: 104, G: 172, B: 214, A: 255}},
+			{"Water2", 0.9, color.RGBA{R: 65, G: 125, B: 201, A: 255}},
+			{"Water", 1.0, color.RGBA{R: 56, G: 103, B: 175, A: 255}},
+		},
+		AutoUpdate: true,
 	}
 
-	mapGen.generateNoiseMap()
-
+	//mapGen.GenerateNoiseMap_island()
+	mapGen.GenerateNoiseMap()
 	for !win.Closed() {
 		if mapGen.AutoUpdate {
-			mapGen.generateNoiseMap()
+			mapGen.GenerateNoiseMap_island()
+			//mapGen.GenerateNoiseMap()
+
 		}
-		mapGen.drawNoiseMap(win)
+		mapGen.DrawNoiseMap(win, "ColourMap")
 		win.Update()
+		time.Sleep(time.Millisecond * 1000)
 	}
 }
+
 func Run_2D_Terrain() {
 	pixelgl.Run(run)
 }
--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 * 2D
     * lib - [pixel](https://github.com/faiface/pixel)
+    * func 
+        * GenerateNoiseMap
+        * GenerateNoiseMap_island
+        <div style="display: flex;">
+            <img src="./gif/2d_terrain.gif" alt="terrain" style="width: 33%;">
+            <img src="./gif/2d_island.gif" alt="island" style="width: 33%;">
+        </div>
 * 3D
     * lib - [g3n](https://github.com/g3n/engine)
